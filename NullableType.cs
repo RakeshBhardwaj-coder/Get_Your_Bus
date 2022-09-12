1422bbe6e85a900154b6a598db3e961e537c8b0f
Rakesh Bhardwaj <57101915+RakeshBhardwaj-coder@users.noreply.github.com>
2022-09-12 19:36:45 +0100

    Create NullableType.cs

---
+++ NullableType.cs
@@ -0,0 +1,25 @@
+public class NullableType : MonoBehaviour
+{
+    void Update()
+    {
+        CreateUser(null, null, true);
+    }
+
+    void CreateUser(string? name, int? age, bool? isCool)
+    {
+        name = name ?? "Unknown"; // if name is null then use as Unknown
+        age = age ?? 18; //if age is null then use age = 18;
+        isCool = isCool ?? false;
+        Debug.Log("Name : " + name);
+        Debug.Log(name + " Age is :" + age);
+        
+
+        if(isCool == false){
+            Debug.Log(name + " is Cool");
+        }else
+        {
+            Debug.Log(name + " is not Cool"); 
+        }
+        
+    }
+}
